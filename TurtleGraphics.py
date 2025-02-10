5e1cf56dc4b93b536d94085634e9c73a8880bf91
BrooksConway <brooksconway@unomaha.edu>
2025-02-10 11:46:36 -0600

    In progress

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -1,16 +1,21 @@
 #TurtleGraphics.py
-#Name:
-#Date:
+#Name:Brooks Conway
+#Date:2/10/25
 #Assignment:
 
 import turtle #needed generally but not in CodeHS
 hideturtle() #hides the default turtle in CodeHS
 
+def drawSquare(myTurtle):
+  for i in range(4):
+    myTurtle.forward(100)
+    myTurtle.right(90)
+bob = turtle.Turtle()
+drawSquare(bob)
 def drawSquare(myTurtle, size):
-    for i in range(4):
-        myTurtle.forward(size)
-        myTurtle.right(90)
-
+  for i in range(4):
+    myTurtle.forward(size)
+    myTurtle.right(90)
 
 def main():
     myTurtle = turtle.Turtle()
